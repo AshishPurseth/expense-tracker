26424c68714b13ca131bec042df30e66c68343f3
ashish purseth <ashish.purseth.external@airbus.com>
2025-07-14 14:52:46 +0530

    update db server

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,12 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+// src/utils/db.server.ts (or similar)
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+import type { PrismaClient as PrismaClientType } from '@prisma/client'
 
 declare global {
     // eslint-disable-next-line no-var
-    var __db: PrismaClient | undefined
+    var __db: PrismaClientType | undefined
 }
 
 const prisma = process.env.NODE_ENV === 'production' ? new PrismaClient() : (global.__db ??= new PrismaClient())
 
-if (process.env.NODE_ENV !== 'production') global.__db = prisma
+if (process.env.NODE_ENV !== 'production') {
+    global.__db = prisma
+}
 
 export { prisma }
