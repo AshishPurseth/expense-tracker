a05bb6395db2a2364d8db2a27549a9b2d864f7d9
ashish purseth <ashish.purseth.external@airbus.com>
2025-07-14 14:01:02 +0530

    fix prisma db

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import pkg from '@prisma/client'
+
+const { PrismaClient } = pkg
+type PrismaClientType = typeof PrismaClient
 
 declare global {
     // eslint-disable-next-line no-var
-    var __db: PrismaClient | undefined
+    var __db: InstanceType<PrismaClientType> | undefined
 }
 
-const prisma = process.env.NODE_ENV === 'production' ? new PrismaClient() : (global.__db ??= new PrismaClient())
+const prisma = process.env.NODE_ENV === 'production' ? new PrismaClient() : (global.__db ?? new PrismaClient())
 
 if (process.env.NODE_ENV !== 'production') global.__db = prisma
 
